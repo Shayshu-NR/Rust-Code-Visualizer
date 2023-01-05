24a64e9ce7895b3ef60e149bdbd98b63be4ebcdc
Rehaan <rckhewamallika@gmail.com>
2023-01-05 15:09:44 -0500

    Wrong file push

--- extension_scripts/profiler.py
+++ extension_scripts/profiler.py
@@ -105,21 +105,33 @@ class profiler:
 
     # Creates a JSON file and writes the data into it
     def __create_json(self):
-        func_labels = list(self.__func_dict.keys())
+        graph_data = ["L1 Data Cache Misses", "LL Data Cache Misses", "Instruction Count", "Branch mispredictions"]
+        func_labels = list(self.__func_dict.keys())[1:]
         graph_dict = {}
-        graph_dict["L1 Data Cache Misses"] = {}
-        graph_dict["L1 Data Cache Misses"]["labels"] = func_labels
-        graph_dict["L1 Data Cache Misses"]["datasets"] = {}
-        graph_dict["L1 Data Cache Misses"]["datasets"]["label"] = "L1 Data Cache Misses"
-        graph_data_list = []
-        for func in func_labels:
-            graph_data_list.append(self.__func_dict[func]["First level data cache read misses"] + self.__func_dict[func]["First level data cache write misses"]) 
+        for dataset in graph_data:
+            graph_dict[dataset] = {}
+            graph_dict[dataset]["labels"] = func_labels
+            graph_dict[dataset]["datasets"] = []
+            graph_dict[dataset]["datasets"].append({})
+            graph_dict[dataset]["datasets"][0]["label"] = dataset
+            graph_data_list = []
+            if dataset == "L1 Data Cache Misses":
+                for func in func_labels:
+                    graph_data_list.append(int(self.__func_dict[func]["First level data cache read misses"]) + int(self.__func_dict[func]["First level data cache write misses"])) 
+            elif dataset == "LL Data Cache Misses":
+                for func in func_labels:
+                    graph_data_list.append(int(self.__func_dict[func]["Last level data cache read misses"]) + int(self.__func_dict[func]["Last level data cache write misses"]))
+            else:
+                for func in func_labels:
+                    graph_data_list.append(int(self.__func_dict[func][dataset]))
         
-        print(graph_data_list)
-        graph_dict["L1 Data Cache Misses"]["datasets"]["data"] = graph_data_list
+            graph_dict[dataset]["datasets"][0]["data"] = graph_data_list
 
         with open('profiler_graphs.json', 'w') as f:
             json.dump(graph_dict, f,indent = 4,)
+
+        with open('profiling_data.json', 'w') as f:
+            json.dump(self.__func_dict, f,indent = 4,)
 
     def __print_info(self):
         for key in self.__func_dict.keys():
