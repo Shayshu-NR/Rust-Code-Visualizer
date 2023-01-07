bded5b126665d35b7f9128e1b23d8b2356d98e42
mattlourenco27 <matt.lourenco27@gmail.com>
2023-01-06 23:37:42 -0500

    updated converter to prepare for filtering

--- converter/converter.py
+++ converter/converter.py
@@ -1,15 +1,29 @@
-def dot_to_json(file_in):
-    import networkx
-    from networkx.readwrite import json_graph
-    import pydot
-    graph_netx = networkx.drawing.nx_pydot.read_dot(file_in)
-    graph_json = json_graph.node_link_data( graph_netx )
-    return json_graph.node_link_data(graph_netx)
+import networkx as nx
+from networkx.readwrite import json_graph
+import pygraphviz as pgv
+import json
+import os
+
+def filter_graph(agraph):
+
+    # Get list of all node names (number) : agraph.nodes()
+
+    # Delete a node by name (number) : agraph.delete_node(n)
+
+    # Get node by name (number) : agraph.get_node(n)
+
+    # get node label : node.attr["label"]
+
+    return agraph #pgv.AGraph
 
 def json_to_file(json_data, file_name="cg.json"):
-    import json
     with open(f'./data/{file_name}', "w") as write_file:
         json.dump(json_data, write_file, indent=4)
 
-file = input("Enter the file path:")
-json_to_file(dot_to_json(file))
+file = input("Enter the file path: ")
+if not os.path.isfile(file):
+    raise RuntimeError(f"file '{file}' does not exist.")
+G = pgv.AGraph(file)
+G = filter_graph(G)
+nx_graph = nx.nx_agraph.from_agraph(G)
+json_to_file(json_graph.cytoscape_data(nx_graph))
